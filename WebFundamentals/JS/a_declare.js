f6428a1347b772eec4d91b58748b16c81896b251
PJ <parirajan.ravindran@sonydadc.com>
2017-11-15 13:34:15 -0800

    I Do Declare Assignment

---
+++ WebFundamentals/JS/a_declare.js
@@ -0,0 +1,30 @@
+/* 4 Numbers */
+var num = 1;
+var num1 = -1;
+var num3 = 1.2;
+var num4 = -2.2;
+console.log(num, num1, num3, num4);
+
+
+/* Strings */
+var FirstName = "Parirajan"
+var LastName = "Ravindran"
+var MidInitial = "J"
+console.log (FirstName+ ' ' + ' ' + MidInitial + ' ' + LastName);
+
+/* Boolean */
+console.log(Boolean('0'));
+console.log(Boolean('1'));
+console.log( Boolean(0) );
+console.log( Boolean(1) );
+console.log( Boolean(10>9) );
+
+/* Undefined */
+var a;
+console.log(typeof(a));
+var f = 2;
+console.log(typeof(f));
+var n = "testing";
+console.log(typeof(n));
+Boolean(10 > 9);
+console.log(typeof(Boolean));
