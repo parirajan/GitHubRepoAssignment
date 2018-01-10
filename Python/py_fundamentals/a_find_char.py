7efd801de8428efb8034fb463eb8e9125bf50a6a
PJ <parirajan.ravindran@sonydadc.com>
2018-01-09 22:58:10 -0800

    Python - Find Characters

--- Python/py_fundamentals/a_find_char.py
+++ Python/py_fundamentals/a_find_char.py
@@ -1,5 +1,20 @@
-word_list = ['hello','world','my','name','is','Anna']
-char = 'o'
+#word_list = ['hello','world','my','name','is','Anna']
+#char = 'o'
+
+word_list = []
+wl = 0
+
+el = raw_input("Enter the Number of Elements in the Word_List: ")
+while wl != int(el):
+  li = raw_input("Enter the Element to the Word_list: ")
+  word_list.append(li)
+  wl = wl + 1
+    
+print "word_list = ", word_list
+
+char = raw_input("Enter the character to find in the elements in word_list: ")
+
+print "char = ", char
 
 new_list = []
 index = 0
