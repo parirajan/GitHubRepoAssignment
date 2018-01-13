409386a8f5d9a35d663545c0a5172c397d2e516e
PJ <parirajan.ravindran@sonydadc.com>
2018-01-12 18:04:09 -0800

    Python - Turtle

---
+++ Python/py_fundamentals/a_turtle.py
@@ -0,0 +1,11 @@
+import turtle
+
+turtle.penup()
+for i in range(1, 50, 5):
+    turtle.right(90)    # Face South
+    turtle.forward(i)   # Move one radius
+    turtle.right(270)   # Back to start heading
+    turtle.pendown()    # Put the pen back down
+    turtle.circle(i)    # Draw a circle
+    turtle.penup()      # Pen up while we go home
+    turtle.home()       # Head back to the start pos
