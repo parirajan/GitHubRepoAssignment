18a0bf5707a2421dddd152b0de9f6a5ac4070732
PJ <parirajan.ravindran@sonydadc.com>
2018-01-22 13:46:17 -0800

    Python - Flask - Disappearing Ninja

---
+++ Python/Flask_Fundamentals/a_disappearing_ninjas/server.py
@@ -0,0 +1,19 @@
+from flask import Flask, redirect, render_template, session
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/ninja')
+def ninja():
+    displayAll = True
+    return render_template('ninja.html', displayAll=displayAll)
+
+@app.route('/ninja/<color>')
+def getColor(color):
+    displayAll = False
+    return render_template('ninja.html', color=color, displayAll=displayAll)
+
+app.run(host='0.0.0.0',port=9090,debug=True)
+
