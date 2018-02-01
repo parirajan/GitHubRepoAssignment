591342d5a3b02d0c4e80f6e8f8762f96d10daf95
PJ <parirajan.ravindran@sonydadc.com>
2018-01-31 22:29:55 -0800

    MySQL Connection Errors

---
+++ Python/MySQL_Flask/a_mysql_conn_err/server.py
@@ -0,0 +1,9 @@
+from flask import Flask
+# import the Connector function
+from mysqlconnection import MySQLConnector
+app = Flask(__name__)
+# connect and store the connection in "mysql" note that you pass the database name to the function
+mysql = MySQLConnector(app, 'mydb')
+# an example of running a query
+print mysql.query_db("SELECT * FROM users")
+app.run(host='0.0.0.0', port=9090,debug=True)
