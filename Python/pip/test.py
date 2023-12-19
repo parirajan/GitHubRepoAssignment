e1da1a3d887a6f4250b5030272d38aa506b982d1
parirajan <parirajan.ravindran@sonydadc.com>
2023-12-19 14:03:58 -0800

    Update test.py

--- Python/pip/test.py
+++ Python/pip/test.py
@@ -1,6 +1,6 @@
 import logging
 import time
-import subprocess
+from systemd import dbus
 
 def wait_for_service_active(service_name, timeout=300, check_interval=10):
     """
@@ -13,25 +13,37 @@ def wait_for_service_active(service_name, timeout=300, check_interval=10):
     """
     logging.basicConfig(level=logging.INFO)
 
+    # Establish a connection to the systemd D-Bus
+    bus = dbus.SystemBus()
+
+    # Access the systemd1 manager interface
+    systemd_manager = dbus.Interface(bus.get_object("org.freedesktop.systemd1", "/org/freedesktop/systemd1"), 
+                                     dbus_interface="org.freedesktop.systemd1.Manager")
+
     start_time = time.time()
     while time.time() - start_time < timeout:
         try:
-            # Use systemctl to check the service status
-            result = subprocess.run(["systemctl", "is-active", service_name], capture_output=True, text=True)
-            if result.stdout.strip() == "active":
+            unit_path = systemd_manager.GetUnit(f"{service_name}.service")
+            unit_proxy = bus.get_object("org.freedesktop.systemd1", unit_path)
+            unit_properties = dbus.Interface(unit_proxy, dbus_interface='org.freedesktop.DBus.Properties')
+
+            # Check the ActiveState property
+            active_state = unit_properties.Get("org.freedesktop.systemd1.Unit", "ActiveState")
+            
+            if active_state == "active":
                 logging.info(f"Service {service_name} is now active.")
                 return True
             else:
                 logging.info(f"Service {service_name} is not active. Checking again in {check_interval} seconds.")
-        except Exception as e:
-            logging.error(f"An error occurred while checking the service status: {e}")
-            return False
+                time.sleep(check_interval)
 
-        time.sleep(check_interval)
+        except Exception as e:
+            logging.error(f"An error occurred: {e}")
+            return False
 
     logging.warning(f"Service {service_name} did not become active within {timeout} seconds.")
     return False
 
 # Example usage
-service_active = wait_for_service_active("your_service_name.service")
+service_active = wait_for_service_active("your_service_name")
 print("Service is active:", service_active)
