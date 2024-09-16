57efd6ad98d49329c1e442a5c4a60e1d138582f0
parirajan <parirajan.ravindran@sonydadc.com>
2024-09-16 14:56:55 -0700

    Update utils.py

--- Python/tls/utils.py
+++ Python/tls/utils.py
@@ -2,12 +2,10 @@ import json
 import logging
 
 def load_config(path='config.json'):
-    """Load and return the JSON configuration."""
     with open(path, 'r') as file:
         return json.load(file)
 
 def setup_logger():
-    """Configure and return a logger."""
     logger = logging.getLogger('SaferPaymentsLogger')
     logger.setLevel(logging.DEBUG)
     handler = logging.StreamHandler()
@@ -16,12 +14,7 @@ def setup_logger():
     logger.addHandler(handler)
     return logger
 
-def get_tls_options(config):
-    """Return TLS options from the config if TLS is enabled."""
-    tls = config.get('tls_validation', {})
-    if tls.get('enabled', False):
-        return {
-            'verify': tls.get('verify', False),
-            'cert': (tls.get('clientCert'), tls.get('clientKey'))
-        }
-    return {'verify': False}
+def prepare_url(config):
+    base_url = f"{config['target']['protocol']}://{config['target']['ip']}:{config['target']['api_port']}"
+    request_type = config.get('request_type', 'postRequest')
+    return f"{base_url}/?{{\"request\":\"{request_type}\"}}"
