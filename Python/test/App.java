2e54f7d133b781f4b256c6b0b4797cf26db23764
parirajan <parirajan.ravindran@sonydadc.com>
2024-09-25 15:05:13 -0700

    Create App.java

---
+++ Python/test/App.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+    @Autowired
+    private ApiService apiService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String response = apiService.sendRequest();
+        System.out.println("Response from server: " + response);
+    }
+}
