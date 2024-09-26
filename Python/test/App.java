121587602562b9e42823f97854e21ef37a5624e4
parirajan <parirajan.ravindran@sonydadc.com>
2024-09-25 21:10:04 -0700

    Update App.java

--- Python/test/App.java
+++ Python/test/App.java
@@ -1,23 +1,42 @@
-package org.company.group;
+package com.example;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.HttpClients;
+import org.apache.hc.client5.http.classic.CloseableHttpClient;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.ClassicHttpResponse;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
-@SpringBootApplication
-public class App implements CommandLineRunner {
-
-    @Autowired
-    private ApiService apiService;
-
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+        String url = "http://url:port/?";
+        String queryParam = "{\"request\": \"ping\"}";
 
-    @Override
-    public void run(String... args) throws Exception {
-        String response = apiService.sendRequest();
-        System.out.println("Response from server: " + response);
+        try {
+            // URL encode the query parameter
+            String encodedQuery = URLEncoder.encode(queryParam, StandardCharsets.UTF_8.toString());
+
+            // Full URL with encoded query
+            String fullUrl = url + encodedQuery;
+
+            // Create HttpClient instance
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+
+            // Create HttpGet request
+            HttpGet request = new HttpGet(fullUrl);
+
+            // Execute the request and get the response
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                // Print the response
+                String responseBody = EntityUtils.toString(response.getEntity());
+                System.out.println("Response: " + responseBody);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
