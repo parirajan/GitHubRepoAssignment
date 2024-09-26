3827fed507f4d29e500a92e1ae32f6372da5e692
parirajan <parirajan.ravindran@sonydadc.com>
2024-09-25 18:19:09 -0700

    Update RestTemplateConfig.java

--- Python/test/RestTemplateConfig.java
+++ Python/test/RestTemplateConfig.java
@@ -1,4 +1,4 @@
-package com.example;
+package org.company.group;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
@@ -6,27 +6,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.core5.ssl.SSLContextBuilder;
+import org.apache.hc.core5.ssl.NoopHostnameVerifier;
+import org.apache.hc.core5.ssl.TrustAllStrategy;
+
 import javax.net.ssl.SSLContext;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.conn.ssl.TrustAllStrategy;
-import org.apache.http.ssl.SSLContextBuilder;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) throws Exception {
+        // Create an SSLContext that accepts all certificates (TrustAllStrategy)
         SSLContext sslContext = SSLContextBuilder.create()
             .loadTrustMaterial(new TrustAllStrategy())
             .build();
 
+        // Create an HttpClient with the custom SSLContext
         CloseableHttpClient httpClient = HttpClients.custom()
             .setSSLContext(sslContext)
             .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
             .build();
 
+        // Set the HttpClient in the RestTemplate
         HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
         return builder.requestFactory(() -> factory).build();
     }
