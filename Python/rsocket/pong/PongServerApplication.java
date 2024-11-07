2a551a1a468a354c5d2ee273999090315dc6f127
parirajan <parirajan.ravindran@sonydadc.com>
2024-11-07 10:03:05 -0800

    Create PongServerApplication.java

---
+++ Python/rsocket/pong/PongServerApplication.java
@@ -0,0 +1,11 @@
+package com.mycompany.pongservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PongServerApplication.class, args);
+    }
+}
