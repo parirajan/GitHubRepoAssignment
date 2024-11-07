c4248e274ecc3c5d20379674709f2e06d5b2334e
parirajan <parirajan.ravindran@sonydadc.com>
2024-11-07 15:45:34 -0800

    Update PongServerApplication.java

--- Python/rsocket/pong/PongServerApplication.java
+++ Python/rsocket/pong/PongServerApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Mono;
+import io.rsocket.RSocket;
 import io.rsocket.core.RSocketServer;
 import io.rsocket.transport.netty.server.TcpServerTransport;
 
@@ -16,18 +17,18 @@ public class PongServerApplication {
     }
 
     @Bean
-    public RSocketServer rSocketServer() {
-        return RSocketServer.create()
-            .bindNow(TcpServerTransport.create(7000)); // Binds the server to port 7000
+    public void startRSocketServer() {
+        RSocketServer.create((setup, sendingSocket) ->
+                Mono.just(new PongResponder()))
+                .bind(TcpServerTransport.create(7000))
+                .block(); // Ensure the server runs continuously
     }
 }
 
-@Controller
-class PongController {
-
-    @MessageMapping("ping")
-    public Mono<String> respondToPing(String message) {
-        System.out.println("Received: " + message);
+class PongResponder extends RSocket {
+    @Override
+    public Mono<String> requestResponse(io.rsocket.Payload payload) {
+        System.out.println("Received: " + payload.getDataUtf8());
         return Mono.just("Pong");
     }
 }
