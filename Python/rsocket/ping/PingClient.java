c206c8e54525ce75bc097e0758dd97e7d6b6994a
parirajan <parirajan.ravindran@sonydadc.com>
2024-11-07 16:19:35 -0800

    Update PingClient.java

--- Python/rsocket/ping/PingClient.java
+++ Python/rsocket/ping/PingClient.java
@@ -1,47 +1,43 @@
 package com.mycompany.pingservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.rsocket.RSocketRequester;
-import org.springframework.stereotype.Component;
 import org.springframework.util.MimeTypeUtils;
 import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-public class PingClientApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(PingClientApplication.class, args);
-    }
-}
-
-@Component
-class PingClient implements CommandLineRunner {
+public class PingClientApplication implements CommandLineRunner {
 
     private final RSocketRequester.Builder requesterBuilder;
 
-    public PingClient(RSocketRequester.Builder requesterBuilder) {
+    @Value("${ping.server.host}")
+    private String host;
+
+    @Value("${ping.server.port}")
+    private int port;
+
+    public PingClientApplication(RSocketRequester.Builder requesterBuilder) {
         this.requesterBuilder = requesterBuilder;
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(PingClientApplication.class, args);
+    }
+
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         RSocketRequester requester = requesterBuilder
             .dataMimeType(MimeTypeUtils.TEXT_PLAIN)
-            .tcp("localhost", 7010); // Connect through Envoy sidecar on 7010
+            .tcp(host, port);
 
-        // Infinite loop to continuously send "Ping" messages
-        int count = 1;
-        while (true) {
-            String message = "Ping " + count++;
-            Mono<String> response = requester
-                .route("ping")
-                .data(message)
-                .retrieveMono(String.class);
+        Mono<String> response = requester
+            .route("ping")
+            .data("Ping")
+            .retrieveMono(String.class);
 
-            response.subscribe(responseMessage -> System.out.println("Received: " + responseMessage));
-
-            Thread.sleep(2000); // Wait for 2 seconds between pings
-        }
+        response.subscribe(responseMessage -> System.out.println("Received: " + responseMessage));
     }
 }
