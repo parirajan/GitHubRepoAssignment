b1db49394a6c3edb61d41849e9983a34b1a35440
parirajan <parirajan.ravindran@sonydadc.com>
2024-11-14 21:56:25 -0800

    Update HealthController.java

--- Python/rsocket/pong/HealthController.java
+++ Python/rsocket/pong/HealthController.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class HealthCheckServer {
@@ -18,22 +17,13 @@ public class HealthCheckServer {
 @RestController
 class HealthCheckController {
 
-    private final AtomicInteger pingsReceivedCounter;
-    private final AtomicInteger pongsSentCounter;
-    private final String serverNodeId;
-
-    public HealthCheckController(
-            PongServerApplication pongServerApplication,
-            @Value("${health.server.node-id}") String serverNodeId) {
-        this.pingsReceivedCounter = pongServerApplication.pingsReceivedCounter;
-        this.pongsSentCounter = pongServerApplication.pongsSentCounter;
-        this.serverNodeId = serverNodeId;
-    }
+    @Value("${health.server.node-id}")
+    private String serverNodeId;
 
     @GetMapping("/health")
     public String getHealthStatus() {
-        int pingsReceived = pingsReceivedCounter.get();
-        int pongsSent = pongsSentCounter.get();
+        int pingsReceived = PongServerApplication.pingsReceivedCounter.get();
+        int pongsSent = PongServerApplication.pongsSentCounter.get();
         return "Health Status: OK | Server Node ID: " + serverNodeId +
                 " | Pings Received: " + pingsReceived +
                 ", Pongs Sent: " + pongsSent;
