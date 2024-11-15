a99658c51ae3d98c9c402e99067d4bbf95d141fa
parirajan <parirajan.ravindran@sonydadc.com>
2024-11-15 12:04:43 -0800

    Update PingClientApplication.java

--- Python/rsocket/ping/PingClientApplication.java
+++ Python/rsocket/ping/PingClientApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
@@ -12,6 +14,7 @@ import io.rsocket.core.RSocketConnector;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import io.rsocket.Payload;
 import io.rsocket.util.DefaultPayload;
+
 
 import java.time.Duration;
 import java.time.Instant;
@@ -101,7 +104,7 @@ public class PingClientApplication {
 
     @RestController
     class ClientSummaryController {
-
+    
         @GetMapping("/summary")
         public String getClientSummary() {
             int pingsSent = getRecentCount(pingsTimestamps);
