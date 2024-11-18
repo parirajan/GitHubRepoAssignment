0cbaefa0619acd2265039fbf127b414240cccaae
parirajan <parirajan.ravindran@sonydadc.com>
2024-11-18 14:17:30 -0800

    Update PongServerApplication.java

--- Python/rsocket/pong/PongServerApplication.java
+++ Python/rsocket/pong/PongServerApplication.java
@@ -13,16 +13,11 @@ import io.rsocket.SocketAcceptor;
 import io.rsocket.Payload;
 import io.rsocket.util.DefaultPayload;
 
-import java.util.zip.CRC32;
-
 @SpringBootApplication
 public class PongServerApplication {
 
     @Value("${pong.server.port}")
     private int rSocketPort;
-
-    @Value("${pong.server.node-id}")
-    private String serverNodeId;
 
     public static void main(String[] args) {
         SpringApplication.run(PongServerApplication.class, args);
@@ -31,17 +26,14 @@ public class PongServerApplication {
     @Bean
     public CommandLineRunner startRSocketServer() {
         return args -> {
-            // Using DisposableServer with the new Reactor Netty API
             DisposableServer server = RSocketServer.create()
                     .acceptor(SocketAcceptor.forRequestStream(this::handleRequestStream))
                     .bindNow(TcpServerTransport.create(rSocketPort));
 
-            System.out.println("RSocket server running on port " + rSocketPort);
+            System.out.println("RSocket server is running on port " + rSocketPort);
 
-            // Add a shutdown hook for graceful server shutdown
             Runtime.getRuntime().addShutdownHook(new Thread(server::dispose));
 
-            // Keep the server running until terminated
             server.onDispose().block();
         };
     }
@@ -49,15 +41,7 @@ public class PongServerApplication {
     private Flux<Payload> handleRequestStream(Payload payload) {
         String receivedMessage = payload.getDataUtf8();
         System.out.println("Received Ping: " + receivedMessage);
-
-        // Generate a pong response
         String responseMessage = receivedMessage.replace("ping", "pong");
         return Flux.just(DefaultPayload.create(responseMessage));
-    }
-
-    private long calculateChecksum(String data) {
-        CRC32 crc = new CRC32();
-        crc.update(data.getBytes());
-        return crc.getValue();
     }
 }
