b62f9088893be19bf0c529f21179d75535ee9f3f
parirajan <parirajan.ravindran@sonydadc.com>
2025-03-11 00:07:30 -0700

    Update mqcli.java

--- Python/jakarta/jms/mqcli.java
+++ Python/jakarta/jms/mqcli.java
@@ -16,16 +16,15 @@ public class MqHttpServer {
             String mqType = System.getProperty("mq.type", "ibmmq").toLowerCase();
 
             if (mqType.equals("activemq")) {
-                System.out.println("Connecting to ActiveMQ...");
-                jmsClient = new ActiveMqClient("tcp://localhost:61616", "TEST.QUEUE");
+                System.out.println("Initializing ActiveMQ Client...");
+                jmsClient = new ActiveMqClient(); // No parameters needed
             } else {
-                System.out.println("Connecting to IBM MQ...");
+                System.out.println("Initializing IBM MQ Client...");
                 jmsClient = new IbmMqClient("your-mq-host", 1414, "QM1", "DEV.APP.SVRCONN", "TEST.QUEUE");
             }
 
             HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-
-            // Send Message Handler
+            
             server.createContext("/send", exchange -> {
                 if (!"POST".equals(exchange.getRequestMethod())) {
                     sendResponse(exchange, 405, "Use POST to send messages");
@@ -37,17 +36,16 @@ public class MqHttpServer {
                 try {
                     jmsClient.sendMessage(message);
                     sendResponse(exchange, 200, "Message Sent: " + message);
-                } catch (Exception e) {  // Catch generic Exception instead of JMSException
+                } catch (Exception e) {
                     sendResponse(exchange, 500, "Error sending message: " + e.getMessage());
                 }
             });
 
-            // Receive Message Handler
             server.createContext("/receive", exchange -> {
                 try {
                     String response = jmsClient.receiveMessage();
                     sendResponse(exchange, 200, response);
-                } catch (Exception e) {  // Catch generic Exception instead of JMSException
+                } catch (Exception e) {
                     sendResponse(exchange, 500, "Error receiving message: " + e.getMessage());
                 }
             });
@@ -62,7 +60,6 @@ public class MqHttpServer {
         }
     }
 
-    // Utility method to send HTTP responses
     private static void sendResponse(com.sun.net.httpserver.HttpExchange exchange, int statusCode, String response)
             throws IOException {
         exchange.sendResponseHeaders(statusCode, response.length());
