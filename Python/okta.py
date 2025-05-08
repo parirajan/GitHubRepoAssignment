f43d66eccd0a1705a8fdcebe2d29921da9dd0f9e
parirajan <parirajan.ravindran@sonydadc.com>
2025-05-08 14:08:34 -0700

    Delete Python/okta.py

--- Python/okta.py
+++
@@ -1,9 +0,0 @@
-curl -X POST "https://your-okta-domain.com/oauth2/v1/token" \
-  -u "client_id:client_secret" \
-  -H "Content-Type: application/x-www-form-urlencoded" \
-  -d "grant_type=client_credentials&scope=okta.apps.read okta.groups.read okta.users.read"
-
-
-curl -X GET "https://your-okta-domain.com/api/v1/apps" \
-  -H "Authorization: Bearer your-token" \
-  -H "Accept: application/json"
