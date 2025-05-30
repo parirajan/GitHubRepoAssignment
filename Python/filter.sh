f3addcc9456ae49082a1418c7216e5417601125c
parirajan <parirajan.ravindran@sonydadc.com>
2025-05-30 09:42:58 -0700

    Update filter.sh

--- Python/filter.sh
+++ Python/filter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage:
-# ./dedupe_json_by_uid_stream.sh input.json message loadFileJob mandator > output.json
+# ./dedupe_json_by_uid_stream.sh input.json message loadFileJob mandator > filtered.json
 
 input_file="$1"
 shift
@@ -12,39 +12,39 @@ stream_list=$(IFS=, ; echo "${stream_types[*]}")
 
 awk -v streams="$stream_list" '
 BEGIN {
-  # Split stream type list into an array
+  # Load list of streamTypes to deduplicate
   n = split(streams, sArr, /,/)
   for (i = 1; i <= n; i++) {
-    valid_streams[sArr[i]] = 1
+    target_streams[sArr[i]] = 1
   }
 }
 
 /"streamType"/ && /"uid"/ {
+  # Extract streamType
   match($0, /"streamType"[[:space:]]*:[[:space:]]*"[^"]+"/, st)
   match($0, /"uid"[[:space:]]*:[[:space:]]*[0-9]+/, uidmatch)
 
   if (st[0] != "" && uidmatch[0] != "") {
-    # Extract and clean streamType
     split(st[0], a, ":")
     gsub(/"/, "", a[2])
     stream = gensub(/^[ \t]+/, "", "g", a[2])
 
-    # Extract and clean uid
     split(uidmatch[0], b, ":")
     uid = gensub(/^[ \t]+/, "", "g", b[2]) + 0
 
     key = stream "_" uid
 
-    if (stream in valid_streams) {
+    # If it's a target streamType, deduplicate
+    if (stream in target_streams) {
       if (!(key in seen)) {
         seen[key] = 1
         print
       }
-      next
+      next  # Skip duplicates for streamTypes we’re filtering
     }
   }
 
-  # Print all non-target streamType lines
+  # For all other streamTypes (non-target), always print
   print
 }
 ' "$input_file"
