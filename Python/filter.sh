6a8ac5fb7d355080139046e68a3e9c494616fea8
parirajan <parirajan.ravindran@sonydadc.com>
2025-05-30 09:35:52 -0700

    Update filter.sh

--- Python/filter.sh
+++ Python/filter.sh
@@ -1,52 +1,56 @@
 #!/bin/bash
 
 # Usage:
-# ./filter_by_streamtypes.sh message loadFileJob mandator input.json > filtered.json
+# ./dedupe_by_stream_and_uid.sh message loadFileJob mandator input.json > filtered.json
 
-input_file="${@: -1}"                    # last argument is input file
-stream_types=("${@:1:$#-1}")             # all except last arg
+# Extract the input file (last argument)
+input_file="${@: -1}"
+# All other args are streamTypes
+stream_types=("${@:1:$#-1}")
 
-# Pass each stream type as a separate -v var
-awk_args=""
+# Generate -v args for each stream type
+awk_vars=""
 for i in "${!stream_types[@]}"; do
-  awk_args+=" -v s$i=\"${stream_types[$i]}\""
+  awk_vars+=" -v st${i}=${stream_types[$i]}"
 done
 
-awk $awk_args '
+awk $awk_vars '
 BEGIN {
-  # Initialize valid streamType list
+  # Load streamTypes into a lookup table
   for (i = 0; i < 100; i++) {
-    if (("s" i) in ENVIRON) {
-      valid_streams[ENVIRON["s" i]] = 1
-    } else {
-      break
-    }
+    varname = "st" i
+    if (!(varname in ENVIRON)) break
+    valid[ENVIRON[varname]] = 1
   }
 }
 
 /"streamType"/ && /"uid"/ {
+  # Extract streamType
   match($0, /"streamType"[[:space:]]*:[[:space:]]*"[^"]+"/, st)
   match($0, /"uid"[[:space:]]*:[[:space:]]*[0-9]+/, uidmatch)
 
   if (st[0] != "" && uidmatch[0] != "") {
+    # Clean up streamType
     split(st[0], a, ":")
     gsub(/"/, "", a[2])
     stream = gensub(/^[ \t]+/, "", "g", a[2])
 
+    # Clean up uid
     split(uidmatch[0], b, ":")
     uid = gensub(/^[ \t]+/, "", "g", b[2]) + 0
 
     key = stream "_" uid
 
-    if (stream in valid_streams) {
+    if (stream in valid) {
       if (!(key in seen)) {
         seen[key] = 1
         print
       }
-      next
+      next  # skip duplicate for same stream+uid
     }
   }
 
+  # For lines without valid streamType match, print as-is
   print
 }
 ' "$input_file"
