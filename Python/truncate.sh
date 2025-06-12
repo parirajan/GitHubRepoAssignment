cfbdd3e43ed5081313ec38253f18ceceb5008900
parirajan <parirajan.ravindran@sonydadc.com>
2025-06-12 14:35:39 -0700

    Update truncate.sh

--- Python/truncate.sh
+++ Python/truncate.sh
@@ -7,4 +7,4 @@ asadm -e "show statistics like defrag_q" | awk -v ns="test" '
 
 
 
-asadm -e "show statistics like defrag_q" | awk -v ns="your_namespace" '$0 ~ "Namespace "ns" Statistics" { f=1; next } f && /defrag_q/ { print $NF; f=0 }'
+asadm -e "show statistics like defrag_q" | awk -v ns="test" '$0 ~ "Namespace[[:space:]]"ns"[[:space:]]Statistics" { f=1; next } f && /defrag_q/ { print $NF; f=0 }'
