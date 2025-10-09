49d0069da7329620469ca7468ddb338f2f6bb25a
parirajan <parirajan.ravindran@sonydadc.com>
2025-10-09 14:40:53 -0700

    Create consumerapp.java

---
+++ Python/mq-demo/apps/consumer/consumerapp.java
@@ -0,0 +1,7 @@
+package org.demo.consumer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class MqConsumerApplication {
+  public static void main(String[] args) { SpringApplication.run(MqConsumerApplication.class, args); }
+}
